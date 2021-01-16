4d4e25bef0f4c5ed108bc3f25ef903ca20bbcfd4
anishmoktan <anish.tmngo@gmail.com>
2021-01-16 13:59:14 -0500

    first commit

---
+++ Everest_Capital_Users/user_functions.py
@@ -0,0 +1,18 @@
+import boto3
+
+
+class AWS_Manager():
+
+    
+dynamodb = boto3.resource('dynamodb')
+
+dynamoTable = dynamodb.Table('Table_Name')
+
+dynamoTable.put_item(
+    Item =
+        {
+            "username":"Ben",
+       
+         }
+)
+
