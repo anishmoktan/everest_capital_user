da69619d1a3282364556793717ada8be83080586
anishmoktan <anish.tmngo@gmail.com>
2021-03-22 14:27:50 -0400

    took out class

--- Everest_Capital_Users/user_functions.py
+++ Everest_Capital_Users/user_functions.py
@@ -1,18 +1,16 @@
 import boto3
 
 
-class AWS_Manager():
+# class AWS_Manager():
 
-    
+
 dynamodb = boto3.resource('dynamodb')
-
 dynamoTable = dynamodb.Table('Table_Name')
 
 dynamoTable.put_item(
     Item =
         {
             "username":"Ben",
-       
          }
 )
 
